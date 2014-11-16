6d8d7c0b275c461e6dfe62c154038a1c8ef3ccd4
Auster Chen <Auster.s.chen@gmail.com>
2014-11-16 15:27:20 -0800

    nested collection and associations

--- app/assets/javascripts/models/todo.js
+++ app/assets/javascripts/models/todo.js
@@ -1,4 +1,12 @@
 window.Todo.Models.Todo = Backbone.Model.extend({
   //properties here are inserted into the model's prototype
-  urlRoot: "/api/todos"
+  urlRoot: "/api/todos",
+
+  comments: function(){
+    if(!this._comments){
+      this._comments = new Todo.Collections.TodoComments([], { todo: this })
+    }
+
+    return this._comments;
+  }
 });
